5fa60d80ce89263ea1acf5deaadd5868785df0ef
Martin <martinfz@t-online.de>
2022-03-31 19:39:31 +0200

    Adding a facialrecognition Vue project

---
+++ facialrecognition/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Azure Facial Recognition Tester</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
